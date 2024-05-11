5019d188fe9ac49b3a16c900ab1b00df0ac6a52f
yedala <aravindyedala49@gmail.com>
2024-05-11 22:18:04 +0530

    basic navigation after sign in

--- movie-list/src/components/Body.js
+++ movie-list/src/components/Body.js
@@ -1,8 +1,14 @@
-import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import {RouterProvider, createBrowserRouter, useNavigate} from "react-router-dom"
 import Browse from "./Browse"
 import Login from "./Login"
+import { useEffect } from "react";
+import { auth } from "../utils/firebase";
+import {useDispatch}  from "react-redux"
+import { addUser,removeUser } from "../utils/userSlice";
+import {onAuthStateChanged } from "firebase/auth";
 
 const Body = () => {
+    const dispatch = useDispatch();
     const appRoutes = createBrowserRouter([
         {
             path:'/',
@@ -12,7 +18,20 @@ const Body = () => {
             path:'/browse',
             element:<Browse/>
         }
-    ])
+    ]);
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              const {uid,email,displayName} = user.uid;
+              dispatch(addUser({uid:uid,email:email,displayName: displayName}));
+             
+            } else {
+             dispatch(removeUser());
+            
+            }
+          });
+    },[]);
+
   return (
     <RouterProvider router={appRoutes} />
   )
--- movie-list/src/components/Login.js
+++ movie-list/src/components/Login.js
@@ -3,12 +3,15 @@ import Header from "./Header"
 import validation from "../utils/validation";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 import { auth } from "../utils/firebase";
+import {useNavigate} from "react-router-dom"
 
 const Login = () => {
     const [isSignIn, setisSignIn] = useState(true);
     const [errmsg, seterrmsg] = useState(null);
     const email = useRef(null);
     const password = useRef(null);
+    const navigate = useNavigate();
+
     const toggleSignInForm = () => {
         setisSignIn(!isSignIn);
     }
@@ -22,7 +25,7 @@ const Login = () => {
                 .then((userCredential) => {
                     // Signed up 
                     const user = userCredential.user;
-                    console.log(user);
+                    navigate("/browse")
                 })
                 .catch((error) => {
                     const errorCode = error.code;
@@ -36,6 +39,7 @@ const Login = () => {
                 .then((userCredential) => {
                     // Signed in 
                     const user = userCredential.user;
+                    navigate("/browse")
                     // ...
                 })
                 .catch((error) => {
